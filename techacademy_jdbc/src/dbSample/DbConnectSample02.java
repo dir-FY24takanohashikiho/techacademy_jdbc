3bd97232e8095a9396790b4811d25f4b7e17dd1a
dir-FY24takanohashikiho <kiho.takanohashi.x825@dirbato.co.jp>
2024-04-16 12:53:14 +0900

    最初のコミット

--- techacademy_jdbc/src/dbSample/DbConnectSample02.java
+++ techacademy_jdbc/src/dbSample/DbConnectSample02.java
@@ -1,4 +1,4 @@
-package dbsample;
+package dbSample;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DbConnectSample01 {
+public class DbConnectSample02 {
 
     public static void main(String[] args) {
         // データベース接続と結果取得のための変数
@@ -39,7 +39,9 @@ public class DbConnectSample01 {
                 // 取得した値を表示
                 System.out.println(name);
             }
-
+            sql = "update country set Population = 105000 where Code = 'ABW'";
+            int count = stmt.executeUpdate(sql);
+            System.out.println(count);
 
         } catch (ClassNotFoundException e) {
             System.err.println("JDBCドライバーのロードに失敗しました。");
